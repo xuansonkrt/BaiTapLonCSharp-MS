ffa94e52287ac4e8e46beffcb17b876f29daa888
xuansonkrt <43500435+xuansonkrt@users.noreply.github.com>
2018-10-28 22:15:10 +0700

    thêm chức năng tìm kiếm

---
+++ DiemDanhKhuonMat/Formmain/GUI/frmTimKiem.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Formmain.GUI
+{
+    partial class frmTimKiem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTimKiem = new System.Windows.Forms.Label();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.btnTimKiem = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTimKiem
+            // 
+            this.labelTimKiem.AutoSize = true;
+            this.labelTimKiem.Location = new System.Drawing.Point(37, 48);
+            this.labelTimKiem.Name = "labelTimKiem";
+            this.labelTimKiem.Size = new System.Drawing.Size(64, 17);
+            this.labelTimKiem.TabIndex = 0;
+            this.labelTimKiem.Text = "Tìm kiếm";
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Location = new System.Drawing.Point(157, 45);
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(225, 22);
+            this.txtTimKiem.TabIndex = 1;
+            // 
+            // btnTimKiem
+            // 
+            this.btnTimKiem.Location = new System.Drawing.Point(70, 98);
+            this.btnTimKiem.Name = "btnTimKiem";
+            this.btnTimKiem.Size = new System.Drawing.Size(111, 32);
+            this.btnTimKiem.TabIndex = 2;
+            this.btnTimKiem.Text = "Tìm kiếm";
+            this.btnTimKiem.UseVisualStyleBackColor = true;
+            this.btnTimKiem.Click += new System.EventHandler(this.btnTimKiem_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Location = new System.Drawing.Point(229, 98);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(111, 32);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // frmTimKiem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 162);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnTimKiem);
+            this.Controls.Add(this.txtTimKiem);
+            this.Controls.Add(this.labelTimKiem);
+            this.Name = "frmTimKiem";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Tìm kiếm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmTimKiem_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Button btnTimKiem;
+        private System.Windows.Forms.Button btnThoat;
+        public System.Windows.Forms.Label labelTimKiem;
+    }
+}
---
+++ DiemDanhKhuonMat/Formmain/GUI/frmTimKiem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formmain.GUI
+{
+    public partial class frmTimKiem : Form
+    {
+        string key;
+        public frmTimKiem()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            if(txtTimKiem.Text.Trim()=="")
+            {
+                MessageBox.Show("Bạn chưa nhận giá trị.", "Thông báo");
+                ActiveControl = txtTimKiem;
+                return;
+            }
+            key = txtTimKiem.Text.Trim();
+            this.Close();
+        }
+        public string getKey()
+        {
+            return key;
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            key = "";
+            this.Close();
+        }
+
+        private void frmTimKiem_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}
